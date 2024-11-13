a1698c37746b5c153d7104a8e3111ac3a71b7127
Kenneth Kaiser <kk1995@live.dk>
2024-11-13 14:58:48 +0100

    ModelFactory implementation

--- src/main/java/dk/acto/blackdragon/implementation/ModelFactoryImpl.java
+++ src/main/java/dk/acto/blackdragon/implementation/ModelFactoryImpl.java
@@ -1,11 +1,38 @@
 package dk.acto.blackdragon.implementation;
 
+import dk.acto.blackdragon.model.Model;
 import dk.acto.blackdragon.service.ModelFactory;
 import io.vavr.collection.List;
 
 public class ModelFactoryImpl implements ModelFactory {
-    @Override
-    public List parse(String string) {
-        return null;
+    /**
+     * Author: Kenneth Kaiser
+     *
+     * Parsing the string input we got from DataFetcher into a list of Model object.
+     *
+     * @return a Vavr List containing Model objects parsed from the input data
+     */
+    public List<Model> parse(String string) {
+
+        List<Model> models = List.empty();
+
+        // Getting lines from string input by seperating with \n"
+        String[] lines = string.trim().split("\n");
+
+        for (int i = 1; i < lines.length; i++) { // i = 1 to skip the header (id, weight, cost, inventory )
+
+            // Splitting the line with comma
+            String[] values = lines[i].split(",");
+            // Putting in the values and trimming
+            long id = Long.parseLong(values[0].trim());
+            double weight = Double.parseDouble(values[1].trim());
+            int cost = Integer.parseInt(values[2].trim());
+            long inventory = Long.parseLong(values[3].trim());
+
+            // Create a new Model object and adding it to the list
+            models = models.append(new Model(id, cost, inventory, weight));
+        }
+
+        return models;
     }
 }
--- src/test/java/dk/acto/blackdragon/service/ModelFactoryTest.java
+++ src/test/java/dk/acto/blackdragon/service/ModelFactoryTest.java
@@ -1,5 +1,6 @@
 package dk.acto.blackdragon.service;
 
+import dk.acto.blackdragon.implementation.ModelFactoryImpl;
 import dk.acto.blackdragon.model.Model;
 import io.vavr.collection.List;
 import org.testng.ITestContext;
@@ -15,12 +16,8 @@ public class ModelFactoryTest {
 
         String data = String.valueOf(context.getAttribute("data"));
 
-        ModelFactory<Model> subject = new ModelFactory<Model>() {
-            @Override
-            public List<Model> parse(String string) {
-                return null;
-            }
-        };
+        // My implementation
+        ModelFactory<Model> subject = new ModelFactoryImpl();
 
         List<Model> result = subject.parse(data);
         assertNotNull(result);
