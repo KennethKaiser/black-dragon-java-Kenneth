b040de2ea00a031218ad64e8137e3a8f0a979cfd
Kenneth Kaiser <kk1995@live.dk>
2024-11-13 13:56:34 +0100

    Implementation for data fetcher

--- src/main/java/dk/acto/blackdragon/implementation/DataFetcherImpl.java
+++ src/main/java/dk/acto/blackdragon/implementation/DataFetcherImpl.java
@@ -2,11 +2,36 @@ package dk.acto.blackdragon.implementation;
 
 import dk.acto.blackdragon.service.DataFetcher;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class DataFetcherImpl implements DataFetcher {
+
+    /**
+     * Author: Kenneth Kaiser
+     *
+     * Simply gets the content from the URL, reads it line by line and appends to stringbuilder with a new line.
+     *
+     * @return the fetched content as a String, with any trailing newline removed
+     */
     @Override
     public String fetchData(URL url) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line).append("\n"); //Adding a line too much in the end
+            }
+            result = new StringBuilder(result.toString().trim()); // Trimming the result to remove excess line (there is probably a better approach)
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result.toString();
     }
 }
--- src/test/java/dk/acto/blackdragon/service/DataFetcherTest.java
+++ src/test/java/dk/acto/blackdragon/service/DataFetcherTest.java
@@ -1,5 +1,6 @@
 package dk.acto.blackdragon.service;
 
+import dk.acto.blackdragon.implementation.DataFetcherImpl;
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
 
@@ -14,12 +15,8 @@ public class DataFetcherTest {
     @Test(groups = "fetch")
     public void testFetchData(ITestContext context) throws Exception {
 
-        DataFetcher subject = new DataFetcher() {
-            @Override
-            public String fetchData(URL url) {
-                return null;
-            }
-        };
+        // My own implementation
+        DataFetcher subject = new DataFetcherImpl();
 
         String result = subject.fetchData(new URL("https://dragon.acto.dk/test.csv"));
         assertNotNull(result);
